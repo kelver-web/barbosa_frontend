33cbec1b3a643ae4f12a8ea95c8c83e616235ad9
kelver <kelverwt@gmail.com>
2025-08-26 21:41:56 -0300

    modified vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,15 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // separa dependências externas
+          }
+        },
+      },
+    },
+  },
 });
