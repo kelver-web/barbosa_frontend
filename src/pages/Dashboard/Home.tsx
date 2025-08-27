d39c0ab94bcaa45eca721a6557046ef0a3db8186
kelver <kelverwt@gmail.com>
2025-08-26 22:27:39 -0300

    modified title

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <PageMeta
         title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
-        description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        description="Petiscaria e Hamburgueria Barbosa - O melhor da culinária de boteco com um toque especial!"
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
         <div className="col-span-12 space-y-6 xl:col-span-7">
